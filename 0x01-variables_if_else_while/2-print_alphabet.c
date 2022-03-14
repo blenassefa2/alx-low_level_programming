cc0d244b22c7cc47a3adf8f95ea1919aa08777a9
blenassefa2 <blenassefa59@yahoo.com>
2022-03-14 07:54:24 -0700

    third task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - raunning starts here
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int j = 'a'; j <= 'z'; j++)
+		putchar(j);
+	return (0);
+}
