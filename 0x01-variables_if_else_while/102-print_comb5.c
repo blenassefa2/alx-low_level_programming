8749d410d9d897632b5513202e30c9efa692eed3
blenassefa2 <blenassefa59@yahoo.com>
2022-03-14 10:27:40 -0700

    one task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - raunning starts here
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int h, i, j, k;
+
+
+	for (h = '0'; h <= '9'; h++)
+	{
+		for (i = h + 1; i <= '9'; i++)
+		{
+			for (j = i + 1; j <= '9'; j++)
+			{
+				for (k = j + 1; k <= '9'; k++)
+				{
+					if (h != '0' || i != '0' || j != '0' || k != '1')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					putchar(h);
+					putchar(i);
+					putchar(' ');
+					putchar(j);
+					putchar(k);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
