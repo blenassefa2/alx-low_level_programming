ad805bd4e28ba748389b53f2479de1694bc72bea
blenassefa2 <blenassefa59@yahoo.com>
2022-03-16 11:41:34 -0700

    a new task on c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void print_alphabet(void);
+/**
+ * main - checks the code
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ *
+ * Return: void (noting)
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+		putchar(j);
+	putchar('\n');
+	return ();
+}
