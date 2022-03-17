59e53af1df73ffd5b31ed5b53a8cbc6dcf0c25c8
blenassefa2 <blenassefa59@yahoo.com>
2022-03-16 21:34:26 -0700

    a new task on c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,15 @@
+#include <stdio.h>
+/**
+ * print_alphabet - prints alphabets in lowercase
+ *
+ * Return: void (noting)
+ */
 
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+		putchar(j);
+	putchar('\n');
+}
