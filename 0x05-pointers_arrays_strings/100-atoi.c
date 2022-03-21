d2f676525a2e58817a6b2320f2d2e5d0ad446524
blenassefa2 <blenassefa59@yahoo.com>
2022-03-21 10:30:15 -0700

    fixed a bug

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *_atoi - converts the first numbers found in to integers
+ *@s: the input from which an integer is extracted
+ *Return: extracted integer
+ */
+int _atoi(char *s)
+{
+	int sign, digit, ans, x;
+	char *p;
+
+	digit = 0;
+	sign = 1;
+	for (p = s; *p != '\0'; p++)
+	{
+		if (digit > 0 && (*p > '9' || *p < '0'))
+			break;
+		if (*p > '0' && *p < '9')
+			digit++;
+		if (*p == '-')
+			sign *= -1;
+	}
+	x = 1;
+	for (p = p - 1; digit > 0; digit--)
+	{
+		int temp;
+
+		temp = *p + '0';
+		ans += temp * x;
+		x *= 10;
+		p--;
+	}
+	return (sign * ans);
+}
