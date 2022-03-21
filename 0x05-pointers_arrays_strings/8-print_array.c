5c3562c1eea0a425c16796dd2c27b7fa955b12e9
blenassefa2 <blenassefa59@yahoo.com>
2022-03-21 09:30:33 -0700

    2 tasks

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_array - prints an array of integers
+ * @a: reference to the start of the array
+ * @n: number of elements to print
+ * Return: nothing (void)
+ */
+void print_array(int *a, int n)
+{
+	int stop;
+
+	for (stop = a + n; a <= stop; a++)
+	{
+		printf("%i", *a);
+		if (a != stop)
+			printf(", ");
+	}
+	printf("\n");
+}
