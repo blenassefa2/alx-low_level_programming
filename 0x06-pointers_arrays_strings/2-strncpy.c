fb2101c9a200897d194726599b6c9f031a7c0e81
blenassefa2 <blenassefa59@yahoo.com>
2022-03-25 09:52:28 -0700

    1 task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,14 +8,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *temp;
+	int i;
 
-	for (temp = dest; n > 0 && *src != '\0'; src++)
-	{
-		*temp = *src;
-		temp++;
-		n--;
-	}
-	*temp = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
