12883942031a3fc75ca54245249cab6a6a8a0df0
blenassefa2 <blenassefa59@yahoo.com>
2022-03-25 09:10:52 -0700

    1 task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * _strcat - calculates the total length of string
+ * _strncat - calculates the total length of string
  * @dest: a reference to the first char of a string
  * @src: a reference to the additional string
  * @n: the number of characters to concatnate
  * Return: referenct to a string
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	char *temp, *temp2;
 
