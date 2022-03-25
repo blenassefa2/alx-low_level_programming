b1bf638324b253c46cf72847bf5a9b61a1a6cba5
blenassefa2 <blenassefa59@yahoo.com>
2022-03-25 10:07:16 -0700

    1 task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; *s1 != '\0' || *s2 != '\0'; s1++, s2++)
+	for (; *s1 != '\0' || *s2 != '\0'; s1++, s2++)
 	{
 		if (*s1 != *s2)
 			return ((*s1 - *s2) - 1);
+	}
 	return (0);
 }
