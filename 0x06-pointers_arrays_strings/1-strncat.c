aee57a7d91735fd0786495853435b185045cfd68
blenassefa2 <blenassefa59@yahoo.com>
2022-03-25 08:56:47 -0700

    1 task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,9 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 	char *temp2;
 
 	temp = dest;
-	for (temp2 = src; n > 0; temp2++)
+	for (temp2 = src; n >= 0; temp2++)
 	{
 		*temp = *temp2;
+		if (*temp == '\0')
+			break;
 		temp++;
 		n--;
 	}
