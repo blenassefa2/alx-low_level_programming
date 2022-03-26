b6b864d7696728ace02f37cf9663701003ec8fa4
blenassefa2 <blenassefa59@yahoo.com>
2022-03-26 08:00:19 -0700

    1 task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * reverse_array - reverses an array
+ * @a: the array to be reversed
+ * @n: the size of the array
+ * Return: notting
+ */
+void reverse_array(int *a, int n)
+{
+	int *tmp, *tmp2, *c;
+
+	tmp = malloc(sizeof(int) * n);
+	tmp2 = tmp;
+	for (c = a + n; c >= a; c--)
+	{
+		tmp2 = *c;
+		tmp2++;
+	}
+	for (; tmp <= tmp2; tmp++, a++)
+		*a = *tmp;
+}
