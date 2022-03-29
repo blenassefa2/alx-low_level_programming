b67829f74b14044320bfa07e6d1dbd0d0d592084
blenassefa2 <blenassefa59@yahoo.com>
2022-03-28 22:50:20 -0700

    1 task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _memset - function that fills the first n bytes of memory area
+ * @s:the string whose memory area will be replaced
+ * @b:the char that will replace the fields of s
+ * @n:the number of bytes to replace
+ * Return: a referece to the pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *tmp;
+
+	for (tmp = s; n > 0; n--, tmp++)
+		*tmp = b;
+	return (s);
+}
