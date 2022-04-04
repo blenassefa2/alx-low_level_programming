0b66dae632dfaf2957eaeea4f6b9c68e4ba23a8e
blenassefa2 <blenassefa59@yahoo.com>
2022-04-03 23:11:44 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - creates a string and initializies it
+ * @str: the string that will be copied to thenew memory
+ * Return: a reference to the start of new string
+ */
+char *_strdup(char *str)
+{
+	char *ans, *tmp;
+
+	ans = malloc(sizeof(str));
+	if (ans == NULL || str == NULL)
+		return (NULL);
+	for (tmp = ans; str != '\0'; str++, tmp++)
+		*tmp = *str;
+	*tmp = '\0';
+	return (ans);
+}
