6c202cb73b91300e6677e4e6cddba45e0f57ee54
blenassefa2 <blenassefa59@yahoo.com>
2022-04-05 06:00:35 -0700

    cmd line arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints name of program
  *@argc: the number of arguments passed
  *@argv: arguments
- * Return - 0 always (success)
+ * Return: 0 always (success)
  */
-int main (int argc, char *argv[argc])
+int main (int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
