99ae759eb13b32a76ac55881412e0d6d3a2de317
blenassefa2 <blenassefa59@yahoo.com>
2022-04-05 05:48:35 -0700

    cmd line arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed to it
+ *
+ * Return - 0 always (success)
+ */
+int main (int argc, char *argv[])
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments passed to it
+ *
+ * Return - 0 always (success)
+ */
+int main (int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+		printf("%s\n", s[x]);
+	return (0);
+}
