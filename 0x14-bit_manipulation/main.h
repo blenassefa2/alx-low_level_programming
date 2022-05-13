5759c06781d3a7238dc56b6c98223f31960f6e48
blenassefa2 <blenassefa59@yahoo.com>
2022-05-13 15:50:11 +0300

    new classic update

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+unsigned int binary_to_uint(const char *);
+void print_binary(unsigned long int);
+int get_bit(unsigned long int, unsigned int);
+int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *,unsigned int);
+unsigned int flip_bits(unsigned long int, unsigned long int);
+int _atoi(const char *);
+int _putchar(char);
+int get_endiannes(void);
+
+#endif
