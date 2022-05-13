624e7e7f79d7ed8c12a096a7ca1f72540c0bfbd1
blenassefa2 <blenassefa59@yahoo.com>
2022-05-13 16:05:48 +0300

    new classic update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_val = 0;
+	unsigned int dec_val = 0, none = 0;
 
 	if (!b)
-		return (0);
+		return (none);
 
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-			return (0);
+			return (none);
 		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 	return (dec_val);
