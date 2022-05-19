bb5fd864ce47d2f621ad2d602b3cd02fee58deca
blenassefa2 <blenassefa59@yahoo.com>
2022-05-19 08:29:24 +0300

    built new struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+#endif
