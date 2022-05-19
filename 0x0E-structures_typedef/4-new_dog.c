810decb26aa51c69b6bc9d2f3fda3b72d44d760c
blenassefa2 <blenassefa59@yahoo.com>
2022-05-19 10:25:38 +0300

    built new struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog instance
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ *
+ * Return: pointer to newly created dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ret = malloc(sizeof(dog_t));
+
+	ret->name = name;
+	ret->age = age;
+	ret->owner = owner;
+
+	return (ret);
+}
