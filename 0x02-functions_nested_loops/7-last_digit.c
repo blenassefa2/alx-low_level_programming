1259f20c0e0d2e8f83d868f562e3f023d271571c
blenassefa2 <blenassefa59@yahoo.com>
2022-05-27 07:02:00 +0300

    built new struct

---
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -0,0 +1,10 @@
+/**
+ * print_last_digit - returns last digit of an argument
+ * @n: the integer
+ *
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isalpha(int);
 int _abs(int);
 int print_sign(int);
 void _putchar(int);
+int print_last_digit(int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 #endif
