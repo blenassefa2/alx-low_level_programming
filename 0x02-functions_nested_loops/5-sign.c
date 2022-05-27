e74f3d6f2f51d604a6cc6647ad807fa199396c02
blenassefa2 <blenassefa59@yahoo.com>
2022-05-27 06:56:18 +0300

    built new struct

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,14 +8,14 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
 	if (c < 0)
 	{
-		_putchar("-");
+		_putchar('-');
 		return (-1);
 	}
-	_putchar("0");
+	_putchar('0');
 	return (0);
 }
