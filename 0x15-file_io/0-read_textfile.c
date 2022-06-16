3626f35d9ef062aec34ea4544e16a8ef4333022e
blenassefa2 <blenassefa59@yahoo.com>
2022-06-16 12:39:35 +0300

    corrected betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
  *
  * Return: the number of letters printed, or 0 if it faild
  */
-ssize_t read_textfile(const char *filename,size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	int s, t;
@@ -36,11 +36,12 @@ ssize_t read_textfile(const char *filename,size_t letters)
 	close(fd);
 
 	t = write(STDOUT_FILENO, buf, s);
-        if (t < 0)
+	if (t < 0)
 	{
 		free(buf);
 		return (0);
 	}
+
 	free(buf);
 	return (t);
 }
