038fbdfe097c4c441c86323df8a4cd5cf882f016
blenassefa2 <blenassefa59@yahoo.com>
2022-07-01 14:49:58 +0300

    hash table last mandatory task

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes hash table
+ * @ht: the table to be deleted
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *curr, *temp;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		while (curr)
+		{
+			temp = curr;
+			curr = curr->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
