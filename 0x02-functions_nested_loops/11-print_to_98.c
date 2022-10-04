6337b64a443eabe1367be94f979d791291adbf8f
blenassefa2 <blenassefa59@yahoo.com>
2022-10-04 07:35:51 +0300

    revising backwards

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,23 +8,19 @@
 void print_to_98(int n)
 {
 	printf("%i", n);
-	if (n < 98)
-		n++;
-	else if (n > 98)
-		n--;
+	if (n != 98)
+	{
+
+		while (n != 98)
+		{
+			printf(", %i", n);
+			if (n > 98)
+				n--;
+			else if (n < 98)
+				n++;
+		}
+		printf(", %i\n", n);
+	}
 	else
-	{
 		printf("\n");
-		return;
-	}
-	while (n != 98)
-	{
-		printf(", %i", n);
-		if (n > 98)
-			n--;
-		else if (n < 98)
-			n++;
-	}
-	printf(", %i\n", n);
-	return;
 }
