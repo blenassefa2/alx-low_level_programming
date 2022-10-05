382935719d5850dee6b4202a657a4c42fdb85085
blenassefa2 <blenassefa59@yahoo.com>
2022-10-05 11:24:38 +0300

    marching forwards

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,7 +18,7 @@ int jump_search(int *array, size_t size, int value)
 	step = sqrt(size), start = 0, end = step;
 	while (start < size)
 	{
-		printf("Value checked array[%lu] = [%d]", start, array[start]);
+		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
 		if (end < size)
 		{
 			if (array[start] <= value && value <= array[end])
@@ -28,11 +28,13 @@ int jump_search(int *array, size_t size, int value)
 			}
 		}
 		else
+		{
 			if (array[start] <= value)
 			{
 				printf("Value found between indexes [%lu] and [%lu]\n", start, end);
 				break;
 			}
+		}
 		start = end;
 		end = start + step;
 	}
